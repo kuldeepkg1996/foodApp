9a2ffb5c970da69f4c7767c8184a7eaa4aca0adb
kuldeepkg1996 <106830159+kuldeepkg1996@users.noreply.github.com>
2023-04-15 12:30:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from "react";
-import PostFood from "./PostFood";
-import DisplayFood from "./DisplayFood";
-//import Navigation from "./Navigation";
+import "./styles.css";
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/Navbar';
+import AddFood from './components/AddFood';
+import DisplayFood from './components/DisplayFood';
 
 const App = () => {
   return (
-    <>
-      {/* <Navigation/> */}
-      <PostFood/>
-      {/* <DisplayFood/> */}
-    </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path='/addfood' element={<AddFood />} />
+        <Route exact path='/displayfood' element={<DisplayFood />} />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
+
